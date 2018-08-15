49fd2c83301a7e739991b4dd23d6c6583bfaf670
丸山隼 <shun@softbank126017124081.bbtec.net>
2018-08-15 21:36:53 +0900

    test

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -8,7 +8,7 @@ return array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=mietaro',
 			'username'   => 'root',
-			'password'   => 'ecomira',
+			'password'   => '',
 		),
 	),
 );
